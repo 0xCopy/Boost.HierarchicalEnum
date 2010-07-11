bdbfc2d1ccc01dcfa2bee82f7910dad207a56a86
Vincent Jacques <vincent@vincent-jacques.net>
2010-07-11 22:27:36 +0200

    New format for project_description.py

--- project_description.py
+++ project_description.py
@@ -5,83 +5,83 @@ import os.path
 
 import TestGenerator
 
-from ViDE.Project.Description import *
 from ViDE.Project.Artefact import AtomicArtefact
 from ViDE.Project.Action import PythonAction
-from ViDE.Project.RealArtefacts import realHeaders
 
 class GeneratedSource( AtomicArtefact ):
-    def __init__( self, project, test, file, headers ):
+    def __init__( self, test, file, headers ):
         self.__sourceFileName = TestGenerator.generatedFileName( test, file )
-        AtomicArtefact.__init__( self, project, name = self.__sourceFileName,
+
+        realHeaders = []
+        for h in headers:
+            realHeaders.append( GeneratedSource.getOrCreate( test, h, [] ) )
+
+        AtomicArtefact.__init__( self, name = self.__sourceFileName,
             files = [ self.__sourceFileName ],
             dependencies = [],
-            orderOnlyDependencies = headers
+            orderOnlyDependencies = realHeaders,
+            automatic = False
         )
         self.__test = test
         self.__file = file
+
+    @staticmethod
+    def computeName( test, file, headers ):
+        return TestGenerator.generatedFileName( test, file )
 
     def doGetProductionAction( self, predecessors ):
         return PythonAction(
             lambda : TestGenerator.generatedFiles[ self.__file ]( self.__test ),
-            dump = "Generate " + self.__sourceFileName,
+            dump = "Generate " + TestGenerator.generatedFileName( self.__test, self.__file ),
             predecessors = predecessors,
         )
 
-class GeneratedHeader( GeneratedSource ):
-    pass
+    def fileName( self ):
+        return TestGenerator.generatedFileName( self.__test, self.__file )
 
-class GeneratedSourceDescription:
-    def __init__( self, test, file, headers = [] ):
-        self.__test = test
-        self.__file = file
-        self.__headers = headers
+###############################################################################
 
-    def create( self, project ):
-        cls = GeneratedSource
-        if self.__file.endswith( ".hpp" ):
-            cls = GeneratedHeader
-        return cls(
-            project,
-            self.__test,
-            self.__file,
-            realHeaders( project, self.__headers )
-        )
-
-def testArtefacts():
-    return [
-        GeneratedSourceDescription( test, file )
-        for file in TestGenerator.generatedFiles if file.endswith( ".hpp" )
-        for test in TestGenerator.tests
-    ] + [
-        GeneratedSourceDescription( test, file, headers = [ TestGenerator.generatedFileName( test, f ) for f in TestGenerator.generatedFiles if f.endswith( ".hpp" ) ] )
-        for file in TestGenerator.generatedFiles if file.endswith( ".cpp" )
-        for test in TestGenerator.tests
-    ] + [
-        Executable( "test_" + test.name,
-            sources = [ TestGenerator.generatedFileName( test, file ) for file in TestGenerator.generatedFiles if file.endswith( ".cpp" ) ],
-            localLibraries = [ "bhe" ],
-            externalLibraries = [ "boost_unit_test_framework" ],
-        )
-        for test in TestGenerator.tests
-    ]
-
-project = Project(
-    artefacts = [
-        HeaderLibrary( "bhe",
-            headers = AllHppIn( "src/boost" )
-        ),
-        Executable( "example_no_ns",
-            sources = [ "src/example/no_ns.cpp" ],
-            localLibraries = [ "bhe" ],
-            externalLibraries = [ "boost_unit_test_framework" ],
-        ),
-        Executable( "example_ns",
-            sources = [ "src/example/ns.cpp" ],
-            localLibraries = [ "bhe" ],
-            externalLibraries = [ "boost_unit_test_framework" ],
-        )
-    ]
-    +
-    testArtefacts()
+HeaderLibrary( "bhe",
+    publicHeaders = AllHppIn( "src/boost" ),
+    stripFromHeaders = "src/",
+    localLibraries = []
 )
+
+Executable(
+    name = "example_no_ns",
+    sources = [ "src/example/no_ns.cpp" ],
+    localLibraries = [ "bhe" ],
+    externalLibraries = [ "boost_unit_test_framework" ],
+)
+
+Executable(
+    name = "example_ns",
+    sources = [ "src/example/ns.cpp" ],
+    localLibraries = [ "bhe" ],
+    externalLibraries = [ "boost_unit_test_framework" ],
+)
+
+for test in TestGenerator.tests:
+    generatedSources = [ f for f in TestGenerator.generatedFiles if f.endswith( ".cpp" ) ]
+    generatedHeaders = [ f for f in TestGenerator.generatedFiles if f.endswith( ".hpp" ) ]
+
+    for file in generatedHeaders:
+        GeneratedSource(
+            test = test,
+            file = file,
+            headers = []
+        )
+
+    for file in generatedSources:
+        GeneratedSource(
+            test = test,
+            file = file,
+            headers = generatedHeaders
+        )
+
+    Executable(
+        name = "test_" + test.name,
+        sources = [ TestGenerator.generatedFileName( test, f ) for f in generatedSources ],
+        localLibraries = [ "bhe" ],
+        externalLibraries = [ "boost_unit_test_framework" ],
+    )
