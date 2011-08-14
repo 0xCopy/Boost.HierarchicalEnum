be531510dc02646ddf974b82f8a21c354d59b23a
Vincent Jacques <vincent@vincent-jacques.net>
2011-08-14 17:16:05 +0200

    Remove wide source lines

--- src/boost/hierarchical_enum/details/collection.hpp
+++ src/boost/hierarchical_enum/details/collection.hpp
@@ -31,31 +31,53 @@ BOOST_PP_CAT( BOOST_PP_CAT( is_, singular ), _of )
 //************************************************************ VARIADIC_FUNCTIONS
 
 #define BOOST_HE_VARIADIC_PREDICATE( predicate )                                \
-template< typename u, typename v0, BOOST_PP_ENUM_SHIFTED_BINARY_PARAMS( BOOST_HE_MAX_ARITY, typename v, = details::NoneType BOOST_PP_INTERCEPT ) >            \
+template<                                                                       \
+    typename u,                                                                 \
+    typename v0,                                                                \
+    BOOST_PP_ENUM_SHIFTED_BINARY_PARAMS(                                        \
+        BOOST_HE_MAX_ARITY,                                                     \
+        typename v,                                                             \
+        = details::NoneType BOOST_PP_INTERCEPT                                  \
+    )                                                                           \
+>                                                                               \
 struct predicate : ::boost::mpl::and_<                                          \
     details::predicate< u, v0 >,                                                \
-    predicate< u, BOOST_PP_ENUM_SHIFTED_PARAMS( BOOST_HE_MAX_ARITY, v ) >                                                          \
+    predicate< u, BOOST_PP_ENUM_SHIFTED_PARAMS( BOOST_HE_MAX_ARITY, v ) >       \
 >                                                                               \
 {};                                                                             \
                                                                                 \
 template< typename u, typename v0 >                                             \
-struct predicate< u, v0 > :                                  \
+struct predicate< u, v0 > :                                                     \
     details::predicate< u, v0 >                                                 \
 {};
 
 #define BOOST_HE_VARIADIC_COLLECTION( predicate, collection )                   \
-template< typename v0, BOOST_PP_ENUM_SHIFTED_BINARY_PARAMS( BOOST_HE_MAX_ARITY, typename v, = details::NoneType BOOST_PP_INTERCEPT ) >                        \
+template<                                                                       \
+    typename v0,                                                                \
+    BOOST_PP_ENUM_SHIFTED_BINARY_PARAMS(                                        \
+        BOOST_HE_MAX_ARITY,                                                     \
+        typename v,                                                             \
+        = details::NoneType BOOST_PP_INTERCEPT                                  \
+    )                                                                           \
+>                                                                               \
 struct collection : boost::mpl::copy_if<                                        \
     data::all< typename v0::value_type >,                                       \
-    predicate< boost::mpl::_1, BOOST_PP_ENUM_PARAMS( BOOST_HE_MAX_ARITY, v ) >,                                        \
+    predicate< boost::mpl::_1, BOOST_PP_ENUM_PARAMS( BOOST_HE_MAX_ARITY, v ) >, \
     boost::mpl::back_inserter< boost::mpl::vector<> >                           \
     >::type                                                                     \
 {};
 
 #define BOOST_HE_VARIADIC_SIZE( collection )                                    \
-template< typename v0, BOOST_PP_ENUM_SHIFTED_BINARY_PARAMS( BOOST_HE_MAX_ARITY, typename v, = details::NoneType BOOST_PP_INTERCEPT ) >                        \
+template<                                                                       \
+    typename v0,                                                                \
+    BOOST_PP_ENUM_SHIFTED_BINARY_PARAMS(                                        \
+        BOOST_HE_MAX_ARITY,                                                     \
+        typename v,                                                             \
+        = details::NoneType BOOST_PP_INTERCEPT                                  \
+    )                                                                           \
+>                                                                               \
 struct BOOST_PP_CAT( nb_, collection ) : boost::mpl::size<                      \
-    collection< BOOST_PP_ENUM_PARAMS( BOOST_HE_MAX_ARITY, v ) >                                                        \
+    collection< BOOST_PP_ENUM_PARAMS( BOOST_HE_MAX_ARITY, v ) >                 \
 >                                                                               \
 {};
 
